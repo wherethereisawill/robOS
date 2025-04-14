013e41d436dc585abaf0df7a9f06470bba4f3ede
Will Norris <willnorris@Wills-MacBook-Pro.local>
2025-04-14 12:05:41 +0100

    Fix netlify config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,18 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
