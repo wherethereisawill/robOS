9ce13d6d8c17a933aaa9317c8b00852180a161f2
Will Norris <willnorris@Wills-MacBook-Pro.local>
2025-04-14 12:33:34 +0100

    Fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/',
   resolve: {
     alias: {
