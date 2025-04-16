56a713b074161f118a44eddcba529254e4d4b770
Will Norris <willnorris@Wills-MacBook-Pro.local>
2025-04-16 22:26:12 +0100

    fix

--- backend/api.py
+++ backend/api.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://rob-os.netlify.app/"],
+    allow_origins=["http://localhost:5173", "https://rob-os.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
